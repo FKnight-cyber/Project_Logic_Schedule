3f2ebc0ba9e378dc0d4ebe469c0f63004be33e65
raulspinheiro <112182677+raulspinheiro@users.noreply.github.com>
2023-11-27 20:59:07 -0300

    Update solution.py
    Adicionando uma 3ª restrição e adicionando restrição para o Z3 solucionar.

--- solution.py
+++ solution.py
@@ -11,3 +11,13 @@ def schedule_courses(k, m, pairs):
 
     # Cada minicurso deve ser ofertado em no m ́aximo um slot.
     at_most_one_slot = [Sum([If(courses[i * m + j], 1, 0) for j in range(m)]) <= 1 for i in range(k)]
+
+# Minicursos com inscriçõoles em comum não podem ser ofertados no mesmo slot.
+    no_common_registrations = [Not(And(courses[(x - 1) * m + i], courses[(y - 1) * m + i]))
+                               for (x, y) in pairs for i in range(m)]
+
+    # Solucionador do Z3
+    solver = Solver()
+
+    # Adicionando as restrições ao solucionador
+    solver.add(at_least_one_slot + at_most_one_slot + no_common_registrations)
